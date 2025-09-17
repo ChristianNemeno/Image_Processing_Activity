c99c3b70a82d27a59281b0d47cbb6a3d4a33d57d
christian <christiannemeno80@gmail.com>
2025-09-17 21:12:43 +0800

    hist

--- Image_Processing_Activity/Form1.Designer.cs
+++ Image_Processing_Activity/Form1.Designer.cs
@@ -34,31 +34,35 @@
             this.buttonCopy = new System.Windows.Forms.Button();
             this.buttonGreyScale = new System.Windows.Forms.Button();
             this.buttonInversion = new System.Windows.Forms.Button();
+            this.buttonHistogram = new System.Windows.Forms.Button();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
             this.SuspendLayout();
             // 
             // pictureBox1
             // 
             this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.pictureBox1.Location = new System.Drawing.Point(145, 124);
+            this.pictureBox1.Location = new System.Drawing.Point(21, 49);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(186, 141);
+            this.pictureBox1.Size = new System.Drawing.Size(258, 440);
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             // 
             // pictureBox2
             // 
             this.pictureBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.pictureBox2.Location = new System.Drawing.Point(365, 124);
+            this.pictureBox2.Location = new System.Drawing.Point(295, 49);
             this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(176, 141);
+            this.pictureBox2.Size = new System.Drawing.Size(264, 440);
             this.pictureBox2.TabIndex = 1;
             this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
             // 
             // buttonLoad
             // 
-            this.buttonLoad.Location = new System.Drawing.Point(145, 295);
+            this.buttonLoad.Location = new System.Drawing.Point(44, 537);
             this.buttonLoad.Name = "buttonLoad";
             this.buttonLoad.Size = new System.Drawing.Size(75, 65);
             this.buttonLoad.TabIndex = 2;
@@ -68,7 +72,7 @@
             // 
             // buttonCopy
             // 
-            this.buttonCopy.Location = new System.Drawing.Point(259, 295);
+            this.buttonCopy.Location = new System.Drawing.Point(125, 537);
             this.buttonCopy.Name = "buttonCopy";
             this.buttonCopy.Size = new System.Drawing.Size(75, 65);
             this.buttonCopy.TabIndex = 3;
@@ -78,7 +82,7 @@
             // 
             // buttonGreyScale
             // 
-            this.buttonGreyScale.Location = new System.Drawing.Point(365, 295);
+            this.buttonGreyScale.Location = new System.Drawing.Point(206, 537);
             this.buttonGreyScale.Name = "buttonGreyScale";
             this.buttonGreyScale.Size = new System.Drawing.Size(73, 64);
             this.buttonGreyScale.TabIndex = 4;
@@ -88,7 +92,7 @@
             // 
             // buttonInversion
             // 
-            this.buttonInversion.Location = new System.Drawing.Point(464, 295);
+            this.buttonInversion.Location = new System.Drawing.Point(285, 538);
             this.buttonInversion.Name = "buttonInversion";
             this.buttonInversion.Size = new System.Drawing.Size(75, 64);
             this.buttonInversion.TabIndex = 5;
@@ -96,11 +100,32 @@
             this.buttonInversion.UseVisualStyleBackColor = true;
             this.buttonInversion.Click += new System.EventHandler(this.buttonInversion_Click);
             // 
+            // buttonHistogram
+            // 
+            this.buttonHistogram.Location = new System.Drawing.Point(366, 537);
+            this.buttonHistogram.Name = "buttonHistogram";
+            this.buttonHistogram.Size = new System.Drawing.Size(79, 64);
+            this.buttonHistogram.TabIndex = 6;
+            this.buttonHistogram.Text = "Histogram";
+            this.buttonHistogram.UseVisualStyleBackColor = true;
+            this.buttonHistogram.Click += new System.EventHandler(this.buttonHistogram_Click);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox3.Location = new System.Drawing.Point(577, 49);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(323, 440);
+            this.pictureBox3.TabIndex = 7;
+            this.pictureBox3.TabStop = false;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(929, 637);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.buttonHistogram);
             this.Controls.Add(this.buttonInversion);
             this.Controls.Add(this.buttonGreyScale);
             this.Controls.Add(this.buttonCopy);
@@ -111,6 +136,7 @@
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -123,6 +149,8 @@
         private System.Windows.Forms.Button buttonCopy;
         private System.Windows.Forms.Button buttonGreyScale;
         private System.Windows.Forms.Button buttonInversion;
+        private System.Windows.Forms.Button buttonHistogram;
+        private System.Windows.Forms.PictureBox pictureBox3;
     }
 }
 
--- Image_Processing_Activity/Form1.cs
+++ Image_Processing_Activity/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Drawing2D;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -103,6 +104,110 @@ namespace Image_Processing_Activity
                 pictureBox2.SizeMode = PictureBoxSizeMode.StretchImage;
 
             }
+        }
+
+        private void buttonHistogram_Click(object sender, EventArgs e)
+        {
+            if (pictureBox1.Image == null) return;
+
+            Bitmap originalImage = new Bitmap(pictureBox1.Image);
+            Bitmap grayscaleImage = new Bitmap(originalImage.Width, originalImage.Height);
+
+            for (int y = 0; y < originalImage.Height; y++)
+            {
+                for (int x = 0; x < originalImage.Width; x++)
+                {
+                    Color px = originalImage.GetPixel(x, y);
+                    int gray = (px.R + px.G + px.B) / 3;
+                    Color newPx = Color.FromArgb(gray, gray, gray);
+                    grayscaleImage.SetPixel(x, y, newPx);
+                }
+            }
+
+            int[] histogramData = new int[256];
+            for (int y = 0; y < grayscaleImage.Height; y++)
+            {
+                for (int x = 0; x < grayscaleImage.Width; x++)
+                {
+                    histogramData[grayscaleImage.GetPixel(x, y).R]++;
+                }
+            }
+
+            int maxFrequency = 0;
+            foreach (int frequency in histogramData)
+            {
+                if (frequency > maxFrequency)
+                {
+                    maxFrequency = frequency;
+                }
+            }
+
+
+            int graphWidth = 256;
+            int graphHeight = 150; 
+            int marginLeft = 40;  
+            int marginRight = 10;
+            int marginTop = 20;    
+            int marginBottom = 40; 
+
+            int bitmapWidth = graphWidth + marginLeft + marginRight;
+            int bitmapHeight = graphHeight + marginTop + marginBottom;
+
+            Bitmap histogramBitmap = new Bitmap(bitmapWidth, bitmapHeight);
+
+            using (Graphics g = Graphics.FromImage(histogramBitmap))
+            {
+                g.Clear(Color.White);
+
+                for (int i = 0; i < graphWidth; i++)
+                {
+                    float scaledValue = ((float)histogramData[i] / maxFrequency) * graphHeight;
+                    g.DrawLine(Pens.Black,
+                               marginLeft + i,                                 
+                               marginTop + graphHeight,                        
+                               marginLeft + i,                                 
+                               marginTop + graphHeight - scaledValue);         
+                }
+
+                g.DrawLine(Pens.Black, marginLeft, marginTop + graphHeight, marginLeft + graphWidth, marginTop + graphHeight); // X-axis
+                g.DrawLine(Pens.Black, marginLeft, marginTop, marginLeft, marginTop + graphHeight); 
+
+                using (Font axisFont = new Font("Arial", 8))
+                using (Brush textBrush = Brushes.Black)
+                using (StringFormat format = new StringFormat())
+                {
+                    format.Alignment = StringAlignment.Center;
+                    g.DrawString("Intensity", axisFont, textBrush, marginLeft + graphWidth / 2, marginTop + graphHeight + 15, format);
+                    g.DrawString("0", axisFont, textBrush, marginLeft, marginTop + graphHeight + 5);
+                    g.DrawString("128", axisFont, textBrush, marginLeft + 128, marginTop + graphHeight + 5, format);
+                    g.DrawString("255", axisFont, textBrush, marginLeft + 255, marginTop + graphHeight + 5, format);
+
+                    format.Alignment = StringAlignment.Near;
+                    g.DrawString("0", axisFont, textBrush, marginLeft - 15, marginTop + graphHeight - 10, format);
+                    g.DrawString(maxFrequency.ToString(), axisFont, textBrush, marginLeft - 35, marginTop, format);
+
+                    GraphicsState state = g.Save(); 
+                    g.TranslateTransform(15, marginTop + graphHeight / 2); 
+                    g.RotateTransform(-90); 
+                    format.Alignment = StringAlignment.Center;
+                    g.DrawString("Frequency", axisFont, textBrush, 0, 0, format);
+                    g.Restore(state); // Restore back to normal
+                }
+            }
+
+            
+
+                pictureBox2.Image = grayscaleImage;
+                pictureBox2.SizeMode = PictureBoxSizeMode.StretchImage;
+
+                pictureBox3.Image = histogramBitmap;
+                pictureBox3.SizeMode = PictureBoxSizeMode.StretchImage;
+            
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
