88d044a8f10ab1dadd1168a898018f4822ef6f35
christian <christiannemeno80@gmail.com>
2025-09-24 12:31:41 +0800

    ss

--- Image_Processing_Activity/Form2.cs
+++ Image_Processing_Activity/Form2.cs
@@ -1,20 +1,15 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 using WebCamLib;
+// Add this for LockBits
+using System.Drawing.Imaging;
 
 namespace Image_Processing_Activity
 {
     public partial class Form2 : Form
     {
         Bitmap imageA, imageB, resultImage;
-
         private Device webcam;
         private Timer processingTimer;
 
@@ -22,14 +17,11 @@ namespace Image_Processing_Activity
         {
             InitializeComponent();
             processingTimer = new Timer();
-            processingTimer.Interval = 100;
+            processingTimer.Interval = 100; // You can try lowering this to 50 for smoother video
             processingTimer.Tick += ProcessingTimer_Tick;
         }
 
-        private void Form2_Load(object sender, EventArgs e)
-        {
-            
-        }
+        private void Form2_Load(object sender, EventArgs e) { }
 
         private void buttonCamera_Click(object sender, EventArgs e)
         {
@@ -40,23 +32,17 @@ namespace Image_Processing_Activity
                 {
                     webcam = devices[0];
                     webcam.ShowWindow(pictureBoxImage);
-
                     buttonCamera.Text = "Stop Camera";
                     buttonLoadImage.Enabled = false;
                 }
-                else
-                {
-                    MessageBox.Show("No webcam found.");
-                }
+                else { MessageBox.Show("No webcam found."); }
             }
             else
             {
                 processingTimer.Stop();
                 buttonSubtract.Text = "Start Subtract";
-
                 webcam.Stop();
-                webcam = null; 
-                
+                webcam = null;
                 pictureBoxImage.Image = null;
                 buttonCamera.Text = "Start Camera";
                 buttonLoadImage.Enabled = true;
@@ -67,15 +53,14 @@ namespace Image_Processing_Activity
         {
             StopAllProcesses();
             this.Hide();
-            Form form1 = new Form1();
+            var form1 = new Form1();
             form1.ShowDialog();
             this.Close();
         }
 
         private void buttonLoadImage_Click(object sender, EventArgs e)
         {
-            OpenFileDialog ofd = new OpenFileDialog();
-            ofd.Filter = "Image Files|*.jpg;*.jpeg;*.png;*.bmp;*.gif";
+            OpenFileDialog ofd = new OpenFileDialog { Filter = "Image Files|*.jpg;*.jpeg;*.png;*.bmp;*.gif" };
             if (ofd.ShowDialog() == DialogResult.OK)
             {
                 imageB = new Bitmap(ofd.FileName);
@@ -86,12 +71,10 @@ namespace Image_Processing_Activity
 
         private void buttonLoadBackground_Click(object sender, EventArgs e)
         {
-            OpenFileDialog ofd = new OpenFileDialog();
-            ofd.Filter = "Image Files|*.jpg;*.jpeg;*.png;*.bmp;*.gif";
+            OpenFileDialog ofd = new OpenFileDialog { Filter = "Image Files|*.jpg;*.jpeg;*.png;*.bmp;*.gif" };
             if (ofd.ShowDialog() == DialogResult.OK)
             {
                 imageA = new Bitmap(ofd.FileName);
-                
                 imageA = new Bitmap(imageA, pictureBoxImage.Width, pictureBoxImage.Height);
                 pictureBoxBG.Image = imageA;
                 pictureBoxBG.SizeMode = PictureBoxSizeMode.StretchImage;
@@ -100,16 +83,8 @@ namespace Image_Processing_Activity
 
         private void buttonSubtract_Click(object sender, EventArgs e)
         {
-            if (imageA == null)
-            {
-                MessageBox.Show("Please load a background image first.");
-                return;
-            }
-            if (webcam == null && imageB == null)
-            {
-                MessageBox.Show("Please start the camera or load a foreground image.");
-                return;
-            }
+            if (imageA == null) { MessageBox.Show("Please load a background image first."); return; }
+            if (webcam == null && imageB == null) { MessageBox.Show("Please start the camera or load a foreground image."); return; }
 
             if (processingTimer.Enabled)
             {
@@ -123,17 +98,13 @@ namespace Image_Processing_Activity
                     processingTimer.Start();
                     buttonSubtract.Text = "Stop Subtract";
                 }
-                else 
-                {
-                    PerformSubtraction();
-                }
+                else { PerformSubtraction(); }
             }
         }
 
         private void ProcessingTimer_Tick(object sender, EventArgs e)
         {
             if (webcam == null || imageA == null) return;
-
             webcam.Sendmessage();
             IDataObject data = Clipboard.GetDataObject();
             if (data != null && data.GetDataPresent(DataFormats.Bitmap))
@@ -152,31 +123,68 @@ namespace Image_Processing_Activity
                 imageA = new Bitmap(imageA, imageB.Width, imageB.Height);
             }
 
+            
             resultImage = new Bitmap(imageB.Width, imageB.Height);
 
+            int threshold = 10; 
+
             
-            int threshold = 10;
+            Rectangle rect = new Rectangle(0, 0, imageB.Width, imageB.Height);
 
-            for (int x = 0; x < imageB.Width; x++)
+            
+            BitmapData foregroundData = imageB.LockBits(rect, ImageLockMode.ReadOnly, PixelFormat.Format24bppRgb);
+            BitmapData backgroundData = imageA.LockBits(rect, ImageLockMode.ReadOnly, PixelFormat.Format24bppRgb);
+            BitmapData resultData = resultImage.LockBits(rect, ImageLockMode.WriteOnly, PixelFormat.Format24bppRgb);
+
+            
+            IntPtr fgPtr = foregroundData.Scan0;
+            IntPtr bgPtr = backgroundData.Scan0;
+            IntPtr resPtr = resultData.Scan0;
+
+            
+            int bytes = Math.Abs(foregroundData.Stride) * imageB.Height;
+            byte[] fgValues = new byte[bytes];
+            byte[] bgValues = new byte[bytes];
+            byte[] resValues = new byte[bytes];
+
+            
+            System.Runtime.InteropServices.Marshal.Copy(fgPtr, fgValues, 0, bytes);
+            System.Runtime.InteropServices.Marshal.Copy(bgPtr, bgValues, 0, bytes);
+
+            
+            for (int i = 0; i < bytes; i += 3)
             {
-                for (int y = 0; y < imageB.Height; y++)
+                byte blue = fgValues[i];
+                byte green = fgValues[i + 1];
+                byte red = fgValues[i + 2];
+
+                int greenStrength = green - Math.Max(red, blue);
+
+                if (greenStrength > threshold)
                 {
-                    Color pixel = imageB.GetPixel(x, y);
-                    Color backPixel = imageA.GetPixel(x, y);
-
-                    int greenStrength = pixel.G - Math.Max(pixel.R, pixel.B);
-
-                    if (greenStrength > threshold)
-                    {
-                        resultImage.SetPixel(x, y, backPixel);
-                    }
-                    else
-                    {
-                        resultImage.SetPixel(x, y, pixel);
-                    }
+                    
+                    resValues[i] = bgValues[i];         
+                    resValues[i + 1] = bgValues[i + 1]; 
+                    resValues[i + 2] = bgValues[i + 2];
+                }
+                else
+                {
+                    
+                    resValues[i] = blue;
+                    resValues[i + 1] = green;
+                    resValues[i + 2] = red;
                 }
             }
 
+           
+            System.Runtime.InteropServices.Marshal.Copy(resValues, 0, resPtr, bytes);
+
+            
+            imageB.UnlockBits(foregroundData);
+            imageA.UnlockBits(backgroundData);
+            resultImage.UnlockBits(resultData);
+
+           
             pictureBoxSubtract.Image = resultImage;
             pictureBoxSubtract.SizeMode = PictureBoxSizeMode.StretchImage;
         }
@@ -190,7 +198,7 @@ namespace Image_Processing_Activity
         {
             if (webcam != null)
             {
-                processingTimer.Stop();
+                processingTimer?.Stop();
                 webcam.Stop();
                 webcam = null;
             }
